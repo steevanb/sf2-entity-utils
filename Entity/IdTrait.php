127a5262b6ca366355dea6bd5325aeb43674fd72
Steevan BARBOYON <steevan.barboyon@gmail.com>
2016-11-08 15:22:34 +0100

    Link Doctrine issue #6042

--- Entity/IdTrait.php
+++ Entity/IdTrait.php
@@ -7,6 +7,9 @@ use steevanb\EntityUtils\Entity\Accessor\IdAccessorTrait;
 
 /**
  * Add a property $id, and it's accessor (getId())
+ *
+ * /!\ Do not use if you use LazyLoading
+ * @link https://github.com/doctrine/doctrine2/issues/6042
  */
 trait IdTrait
 {
